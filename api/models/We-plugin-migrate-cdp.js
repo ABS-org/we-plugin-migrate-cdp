e92a780f72a24f4c6cea62755eb8c44724818cdb
thbastos <thbastos.web@gmail.com>
2014-11-05 19:14:58 -0200

    init

---
+++ api/models/We-plugin-migrate-cdp.js
@@ -0,0 +1,14 @@
+/**
+ * We-plugin-migrate-cdpModel
+ *
+ * @module      :: Model
+ * @description :: [Add info about you model here]
+ *
+ */
+
+module.exports = {
+  schema: true,
+  attributes: {
+
+  }
+};
