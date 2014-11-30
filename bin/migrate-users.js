cf91a4c9c9cb18775e06e3cf1552542d21294a10
thbastos <thbastos.web@gmail.com>
2014-11-30 20:09:45 -0200

    adicionando loop para registros de todos os usuários do db

--- bin/migrate-users.js
+++ bin/migrate-users.js
@@ -16,92 +16,117 @@ function init() {
     sails.log('Plugin migrate CdP...');
     sails.log('Path cwd: ',cwd);
 
-    var sql = "SELECT "
-                +"users.name AS users_name, "
-                +"users.uid AS uid, "
-                +"users.mail AS users_mail, "
-                +"users.created AS users_created, "
-                +"field_data_field_bio.field_bio_value AS biography, "
-                +"field_data_field_data_de_nascimento.field_data_de_nascimento_day AS birthDate_day, "
-                +"field_data_field_data_de_nascimento.field_data_de_nascimento_month AS birthDate_month, "
-                +"field_data_field_data_de_nascimento.field_data_de_nascimento_year AS birthDate_year, "
-                +"field_data_field_name_first.field_name_first_value AS displayName "
-                +"FROM "
-                  +"users, "
-                  +"field_data_field_bio, "
-                  +"field_data_field_data_de_nascimento, "
-                  +"field_data_field_name_first "
-                +"WHERE "
-                  +"users.status <> '0' "
-                  +"AND users.uid = field_data_field_bio.entity_id "
-                  +"AND users.uid = field_data_field_data_de_nascimento.entity_id "
-                  +"AND users.uid = field_data_field_name_first.entity_id "
-                +"ORDER BY "
-                  +"users_created DESC "
-                +"LIMIT 50 OFFSET 0";
-    
-    var usersToSave =  [];
+    var sqlCountAll = "SELECT name FROM users";
 
-    Drupal.query(sql,function(err, sqlResult ){
+    // Seleciona todos os registros da tabela
+    Drupal.query(sqlCountAll,function(err, sqlResultCount ){
 
       if(err){
-        sails.log.error('Erro Drupal.query: ', err);
+        sails.log.error('Total de linhas encontradas para usuários: ', sqlResultCount.length);
         return doneAll(err);
-      } 
+      }
 
-      var objUser = [];
-      var arrayUser = {};
+      sails.log.info('Valor toral dos registros na tabela de usuários: ', sqlResultCount.length);
 
-      // assuming openFiles is an array of file names 
-      async.each(sqlResult, function( result, done) {
-        // Perform operation on file here.
-        User
-        .findOne({ uid_drupal : result.uid })
-        .exec( function(err, userCreated){
+      // Calcula o total dividido por 50, para fazer o loop de 50 em 50 registros na tabela de usuários
+      var sumTotalFor = Math.round(sqlResultCount.length / 50);
+      sails.log.info('A inserção de 50 em 50 registros será feita '+sumTotalFor+' vezes.');
 
-          if(userCreated){
-            sails.log.info('Usuário '+result.users_name+' já está criado.'); 
-            return done();
-          }
+      for (iReg = 0; iReg < sumTotalFor; iReg++) {
+          var iniConsulta = iReg * 50;
+          var fimConsulta = (iniConsulta + 50) - 1;
+          sails.log.info('Migrando os registros... iniciando no registro '+iniConsulta+' até '+fimConsulta+'.');
 
-          User.create({
-            'password' : 123456,
-            'uid_drupal' : result.uid,
-            'username' : result.users_name.toString(),
-            'biography' : result.biography,
-            'email' : result.users_mail.toString(),
-            'displayName' : result.displayName,
-            'birthDate' : result.birthDate_year+'-'+result.birthDate_month+'-'+result.birthDate_day
-          }).exec(function(err, newUser) {
-            if(err){
-              sails.log.error('Erro User.create: ', err);
-              return done(err);
-            }
-            sails.log.info('newUser: ', newUser);
-            DrupalMigrate.create({
-              'uid_usuario_drupal': newUser.uid_drupal,
-              'id_creator': newUser.id,
-              'uid_conteudo_drupal': '',
-              'id_conteudo_wejs': '',
-              'type': 'user'
-            }).exec(function(err, newMigrate){
-              if(err){
-                sails.log.error('Erro DrupalMigrate.create: ', err);
-                return done(err);
+          var sql = "SELECT "
+                    +"users.name AS users_name, "
+                    +"users.uid AS uid, "
+                    +"users.mail AS users_mail, "
+                    +"users.created AS users_created, "
+                    +"field_data_field_bio.field_bio_value AS biography, "
+                    +"field_data_field_data_de_nascimento.field_data_de_nascimento_day AS birthDate_day, "
+                    +"field_data_field_data_de_nascimento.field_data_de_nascimento_month AS birthDate_month, "
+                    +"field_data_field_data_de_nascimento.field_data_de_nascimento_year AS birthDate_year, "
+                    +"field_data_field_name_first.field_name_first_value AS displayName "
+                    +"FROM "
+                      +"users, "
+                      +"field_data_field_bio, "
+                      +"field_data_field_data_de_nascimento, "
+                      +"field_data_field_name_first "
+                    +"WHERE "
+                      +"users.status <> '0' "
+                      +"AND users.uid = field_data_field_bio.entity_id "
+                      +"AND users.uid = field_data_field_data_de_nascimento.entity_id "
+                      +"AND users.uid = field_data_field_name_first.entity_id "
+                    +"ORDER BY "
+                      +"users_created DESC "
+                    +"LIMIT "+iniConsulta+","+fimConsulta+"";
+        
+        var usersToSave =  [];
+
+        Drupal.query(sql,function(err, sqlResult ){
+
+          if(err){
+            sails.log.error('Erro Drupal.query: ', err);
+            return doneAll(err);
+          } 
+
+          var objUser = [];
+          var arrayUser = {};
+
+          // assuming openFiles is an array of file names 
+          async.each(sqlResult, function( result, done) {
+            // Perform operation on file here.
+            User
+            .findOne({ uid_drupal : result.uid })
+            .exec( function(err, userCreated){
+
+              if(userCreated){
+                sails.log.info('Usuário '+result.users_name+' já está criado.'); 
+                return done();
               }
-              sails.log.info('newMigrate: ', newMigrate);
-              done();
+
+              User.create({
+                'password' : 123456,
+                'uid_drupal' : result.uid,
+                'username' : result.users_name.toString(),
+                'biography' : result.biography,
+                'email' : result.users_mail.toString(),
+                'displayName' : result.displayName,
+                'birthDate' : result.birthDate_year+'-'+result.birthDate_month+'-'+result.birthDate_day
+              }).exec(function(err, newUser) {
+                if(err){
+                  sails.log.error('Erro User.create: ', err);
+                  return done(err);
+                }
+                sails.log.info('newUser: ', newUser);
+                DrupalMigrate.create({
+                  'uid_usuario_drupal': newUser.uid_drupal,
+                  'id_creator': newUser.id,
+                  'uid_conteudo_drupal': '',
+                  'id_conteudo_wejs': '',
+                  'type': 'user'
+                }).exec(function(err, newMigrate){
+                  if(err){
+                    sails.log.error('Erro DrupalMigrate.create: ', err);
+                    return done(err);
+                  }
+                  sails.log.info('newMigrate: ', newMigrate);
+                  done();
+                });
+              });
+
             });
+
+          }, function(err){
+              // if any of the file processing produced an error, err would equal that error
+              sails.log.error('async.each: ', err);
           });
 
-        });
+        }); // End Drupal.query
 
-      }, function(err){
-          // if any of the file processing produced an error, err would equal that error
-          sails.log.error('async.each: ', err);
-      });
+      } // end for
 
-    }); // End Drupal.query
+    });
 
   })
 }
