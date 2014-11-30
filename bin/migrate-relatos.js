835810aca6536eca922c68368a64015290982056
thbastos <thbastos.web@gmail.com>
2014-11-30 21:35:26 -0200

    add consulta sql

--- bin/migrate-relatos.js
+++ bin/migrate-relatos.js
@@ -11,87 +11,125 @@ var loadSails = require(cwd + '/bin/loadSails.js');
 
 function init() {
   return loadSails(function afterLoadSails(err, sails) {
-    sails.log.warn('Plugin migrate CdP...');
+    
+    sails.log('Plugin migrate CdP...');
     sails.log('Path cwd: ',cwd);
 
-    //Converter Class
-    var CSVConverter = require("csvtojson").core.Converter;
-    var fs = require("fs");
+    var sqlCountAll = "SELECT title FROM node WHERE type = 'experiencia'";
 
-    var testData = cwd + "/files/migration/migracao_relatos.csv";
-    var data = fs.readFileSync(testData).toString();
-    var csvConverter = new CSVConverter();
+    // Seleciona todos os registros da tabela
+    Drupal.query(sqlCountAll,function(err, sqlResultCount ){
 
-    //end_parsed will be emitted once parsing finished
-    csvConverter.on("end_parsed", function(jsonObj) {
-        //final result poped here as normal.
-    });
+      if(err){
+        sails.log.error('Total de linhas encontradas para relatos: ', sqlResultCount.length);
+        return doneAll(err);
+      }
 
-    var relatosToSave =  [];
+      sails.log.info('Valor toral dos registros na tabela de relatos: ', sqlResultCount.length);
 
-    csvConverter.fromString(data,function(err, relatoObjs){
-      
-      var jsonObj = null;
+      // Calcula o total dividido por 50, para fazer o loop de 50 em 50 registros na tabela de usuários
+      var sumTotalFor = Math.round(sqlResultCount.length / 50);
+      sails.log.info('A inserção de 50 em 50 registros será feita '+sumTotalFor+' vezes.');
 
-      // Loop in objCsv for construct relato array
-      for (var i = 0; i < relatoObjs.length; i++) {
+      for (iReg = 0; iReg < sumTotalFor; iReg++) {
+          var iniConsulta = iReg * 50;
+          var fimConsulta = (iniConsulta + 50) - 1;
+          sails.log.info('Migrando os registros... iniciando no registro '+iniConsulta+' até '+fimConsulta+'.');
 
-        //sails.log.info('jsonObj: ', relatoObjs[i]);
-        //sails.log.info('relatoObjs keys:', Object.keys(relatoObjs[i]));
+          /*
 
-        User.find()
-        .where({ uid_drupal: relatoObjs[i].uid_usuario })
-        .exec(function (err, response) {
-            sails.log('response find uid_drupal: ', response);
-
-            var arrayRelatoAll = {
-              'creator' : relatoObjs[i].uid_usuario,
-              'uid_usuario' : relatoObjs[i].uid_usuario,
-              'titulo' : relatoObjs[i].titulo,
-              'venda_seu_peixe' : relatoObjs[i].venda_seu_peixe,
-              'para_comecar_imagem' : relatoObjs[i].para_comecar_imagem,
-              'ambito_experiencia' : relatoObjs[i].ambito_experiencia,
-              'topicos' : relatoObjs[i].topicos,
-              'sobre_experiencia' : relatoObjs[i].sobre_experiencia,
-              'como_funcionou' : relatoObjs[i].como_funcionou,
-              'desafios' : relatoObjs[i].desafios,
-              'novidades' : relatoObjs[i].novidades,
-              'campo_livre' : relatoObjs[i].campo_livre,
-              'galeria_imagens' : relatoObjs[i].galeria_imagens,
-              'nome_pessoas' : relatoObjs[i].nome_pessoas,
-              'categorias_relato' : relatoObjs[i].categorias_relato,
-              'nome_pessoas' : relatoObjs[i].nome_pessoas,
-              'categoria_profissao' : relatoObjs[i].categoria_profissao,
-              'pontos_envolvidos' : relatoObjs[i].pontos_envolvidos,
-              'local' : relatoObjs[i].local,
-              'estado' : relatoObjs[i].estado,
-              'comunidades' : relatoObjs[i].comunidades
-            };
-
-        });        
-
-        var arrayRelatoWe = {
-              'uid_usuario' : relatoObjs[i].uid_usuario,
-              'titulo' : relatoObjs[i].titulo,
-              'descricao' : relatoObjs[i].sobre_experiencia,
-              'local' : relatoObjs[i].local
-            };
+          var sql = "SELECT "
+                    +"users.name AS users_name, "
+                    +"users.uid AS uid, "
+                    +"users.mail AS users_mail, "
+                    +"users.created AS users_created, "
+                    +"field_data_field_bio.field_bio_value AS biography, "
+                    +"field_data_field_data_de_nascimento.field_data_de_nascimento_day AS birthDate_day, "
+                    +"field_data_field_data_de_nascimento.field_data_de_nascimento_month AS birthDate_month, "
+                    +"field_data_field_data_de_nascimento.field_data_de_nascimento_year AS birthDate_year, "
+                    +"field_data_field_name_first.field_name_first_value AS displayName "
+                    +"FROM "
+                      +"users, "
+                      +"field_data_field_bio, "
+                      +"field_data_field_data_de_nascimento, "
+                      +"field_data_field_name_first "
+                    +"WHERE "
+                      +"users.status <> '0' "
+                      +"AND users.uid = field_data_field_bio.entity_id "
+                      +"AND users.uid = field_data_field_data_de_nascimento.entity_id "
+                      +"AND users.uid = field_data_field_name_first.entity_id "
+                    +"ORDER BY "
+                      +"users_created DESC "
+                    +"LIMIT "+iniConsulta+","+fimConsulta+"";
         
-        //sails.log.info('arrayRelato:', arrayRelatoWe);
+          var usersToSave =  [];
 
-        relatosToSave.push(arrayRelatoWe);
+          Drupal.query(sql,function(err, sqlResult ){
 
-      } // end for
+            if(err){
+              sails.log.error('Erro Drupal.query: ', err);
+              return doneAll(err);
+            } 
 
-      
-      Relato.create(relatosToSave).exec(function(err, newRecord) {
-        console.log('err: ', err);
-        console.log('newRecord: ', newRecord);
-      });
-      
-      // ao terminar rode o doneAll();
-      //doneAll();
-    });
+            var objUser = [];
+            var arrayUser = {};
+
+            // assuming openFiles is an array of file names 
+            async.each(sqlResult, function( result, done) {
+              // Perform operation on file here.
+              User
+              .findOne({ uid_drupal : result.uid })
+              .exec( function(err, userCreated){
+
+                if(userCreated){
+                  sails.log.info('Usuário '+result.users_name+' já está criado.'); 
+                  return done();
+                }
+
+                User.create({
+                  'password' : 123456,
+                  'uid_drupal' : result.uid,
+                  'username' : result.users_name.toString(),
+                  'biography' : result.biography,
+                  'email' : result.users_mail.toString(),
+                  'displayName' : result.displayName,
+                  'birthDate' : result.birthDate_year+'-'+result.birthDate_month+'-'+result.birthDate_day
+                }).exec(function(err, newUser) {
+                  if(err){
+                    sails.log.error('Erro User.create: ', err);
+                    return done(err);
+                  }
+                  sails.log.info('newUser: ', newUser);
+                  DrupalMigrate.create({
+                    'uid_usuario_drupal': newUser.uid_drupal,
+                    'id_creator': newUser.id,
+                    'uid_conteudo_drupal': '',
+                    'id_conteudo_wejs': '',
+                    'type': 'user'
+                  }).exec(function(err, newMigrate){
+                    if(err){
+                      sails.log.error('Erro DrupalMigrate.create: ', err);
+                      return done(err);
+                    }
+                    sails.log.info('newMigrate: ', newMigrate);
+                    done();
+                  });
+                });
+
+              });
+
+            }, function(err){
+                // if any of the file processing produced an error, err would equal that error
+                sails.log.error('async.each: ', err);
+            });
+
+          }); // End Drupal.query
+          */
+
+        } // end for
+
+    }); // end Drupal.query sqlCountAll
+
   });
 }
 
@@ -107,3 +145,37 @@ function doneAll(err){
 init();
 
 
+/*
+
+Consulta view migracao_relatos
+
+SELECT 
+node.uid AS node_uid, 
+node.status AS node_status, 
+node.title AS node_title, 
+node.nid AS nid, 
+node.created AS node_created, 
+'node' AS field_data_field_descricao_node_entity_type, 
+'node' AS field_data_field_imagem_de_destaque_node_entity_type, 
+'node' AS field_data_field_experiencia_ambito_node_entity_type, 
+'node' AS field_data_field_topics_node_entity_type, 
+'node' AS field_data_field_experiencia_desenvolvida_node_entity_type,
+'node' AS field_data_field_experiencia_funcionamento_node_entity_type, 
+'node' AS field_data_field_experiencia_desafios_node_entity_type, 
+'node' AS field_data_field_experiencia_novidades_node_entity_type, 
+'node' AS field_data_field_experiencia_observacoes_node_entity_type, 
+'node' AS field_data_field_imagem_node_entity_type, 
+'node' AS field_data_field_experiencia_ator_nome_node_entity_type, 
+'node' AS field_data_field_experiencia_ator_categoria_node_entity_type, 
+'node' AS field_data_field_experiencia_autornome_node_entity_type, 
+'node' AS field_data_field_experiencia_autorcat_node_entity_type, 
+'node' AS field_data_field_envolve_quais_pontos_equip_node_entity_type, 
+'node' AS field_data_field_cidade_node_entity_type, 
+'node' AS field_data_field_estado_node_entity_type, 
+'node' AS field_data_og_group_ref_node_entity_type
+FROM 
+{node} node
+WHERE (( (node.status = '1') AND (node.type IN  ('relato_de_experi_ncia_dab_curado')) ))
+ORDER BY node_created DESC
+LIMIT 20 OFFSET 0
+*/
