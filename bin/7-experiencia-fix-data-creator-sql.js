36d942abdff851cedab98529afbbced1fa8d4763
albertosouza <alberto.souza.99@gmail.com>
2014-12-14 22:40:27 -0200

    fix relato galeria query

--- bin/7-experiencia-fix-data-creator-sql.js
+++ bin/7-experiencia-fix-data-creator-sql.js
@@ -75,7 +75,7 @@ function updateRelatoExperienciaRecord(record, done) {
         }
 
         var nid = record.nid;
-        if ( relato.images && relato.images.length) {
+        if ( relato.images && relato.images.length >= 2) {
           nid = null; // skip if already has images 
         }
 
@@ -111,7 +111,7 @@ function updateRelatoExperienciaRecord(record, done) {
 function getGaleriaImages(nid, nodeCreatorId, cb) {
   if(!nid) return cb();
 
-  var sql = "SELECT field.`entity_id` AS nid, field.`field_imagem_de_destaque_fid` AS fid, file.`uri` AS uri FROM `field_data_field_imagem_de_destaque` AS field LEFT JOIN `file_managed` AS file ON file.fid = field.`field_imagem_de_destaque_fid` WHERE field.`entity_id`=" + nid + " AND field.`bundle`='experiencia'";
+  var sql = "SELECT field.`entity_id` AS nid, field.`field_imagem_fid` AS fid, file.`uri` AS uri FROM `field_data_field_imagem` AS field LEFT JOIN `file_managed` AS file ON file.fid = field.`field_imagem_fid` WHERE field.`entity_id`=" + nid + " AND field.`bundle`='experiencia'";
 
   Drupal.query(sql, function(err, recordFiles) {
     if (err) return cb(err);
